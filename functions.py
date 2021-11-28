3ea08324181f27e67c24e742459358b1b8511d2a
natanalbat <sahersa@ac.sce.ac.il>
2021-11-28 16:49:00 +0200

    5ra

--- functions.py
+++ functions.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 root = Tk()
 def openNewWindow():
-    print("dsanidasn")
     newWindow = Toplevel(root)
     newWindow.title("New Window")
     newWindow.geometry("200x200")
