b6c948c304657ae0f293b3e801d3a52ed2a2b167
sahersamara <95171462+sahersamara@users.noreply.github.com>
2021-11-29 16:05:16 +0200

    s
    s

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 import functions
-import Pages
 frm = ttk.Frame(functions.root, padding=100)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=functions.root.destroy).grid(column=1, row=0)
-ttk.Button(frm, text="SignUp", command=functions.openNewWindow).grid(column=2, row=0)
+ttk.Button(frm, text="SignUp", command=lambda :functions.openNewWindow("a")).grid(column=2, row=0)
+# ttk.Button(frm, text="sign in", command=functions.LoginPage).grid(column=3, row=0)
 functions.root.mainloop()
