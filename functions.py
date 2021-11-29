d8a4d132f41551d0fa03e4db2cb3f66bbbc261c9
sahersamara <95171462+sahersamara@users.noreply.github.com>
2021-11-29 11:35:38 +0200

    Revert "n"
    This reverts commit cdce2053c5b936c91da1b600940253ecab9328d2, reversing
    changes made to 3ea08324181f27e67c24e742459358b1b8511d2a.

--- functions.py
+++ functions.py
@@ -1,16 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-<<<<<<< HEAD
 def openNewWindow():
-=======
-
-
-def openNewWindow(a):
-    print(f"{a}")
->>>>>>> main
     newWindow = Toplevel(root)
     newWindow.title("New Window")
     newWindow.geometry("200x200")
     Label(newWindow,
-          text="This is a new window").pack()
+          text ="This is a new window").pack()
