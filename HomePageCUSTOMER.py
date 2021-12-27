ee86c81aa6a86f1b880401eaede2025e193e57ca
sahersamara <95171462+sahersamara@users.noreply.github.com>
2021-12-27 22:20:08 +0200

    1.3

--- HomePageCUSTOMER.py
+++ HomePageCUSTOMER.py
@@ -1,25 +1,74 @@
 from tkinter import *
+from tkcalendar import *
 from functools import partial
 from database import database_connection
 import tkinter as tk
 from functions import *
 from SignIn import *
+import datetime
+
 def PetAgeVaildetor(x):
        count=0
        for i in range (len(x)):
               if(x[i]>'9' or x[i]<'0'):
                      return False
        return True
+
 def Reservation(USER):
-       Pets=getPetsByUSERid(USER.userID)
-       dec=[]
-       for p in range(len(Pets)):
-              dec[Pets[p][0]+'('+Pets[p][1]+')']=Pets[p][2]
+       tktk = Toplevel(root)
+       tktk.title("Reservation")
+       tktk.geometry("1080x1920")
+       Pets=database_connection.getPetsByUSERid(USER.userID)
+       PetsList=[]
+       if(len(Pets)==0):
+              popupmsg('You have no pets ,please add one ')
+              tktk.destroy()
+       for p in Pets:
+              PetsList.append(p[0].replace('(','').replace(')','').split(','))
+       dec={}
+       for p in PetsList:
+              dec[p[0]+'('+p[1]+')']=p[2]
+              print(p[0]+'('+p[1]+')')
+              print(p[2])
+       Pets={}
+       Pets=dec.keys()
+       print(Pets)
        tkvar = StringVar(root)
-       tkvar.set('Dog') # set the default option
+       tkvar.set('None') # set the default option
        # on change dropdown value
        def change_dropdown(*args):
               print( tkvar.get() )
+       label_Type = Label(tktk, text="Pet-('Name'('Type'))", width=20, font=("bold", 10))
+       # label_Type.place(x=80, y=160)
+       label_Type.pack(pady=20)
+
+       text_Type = OptionMenu(tktk, tkvar, *Pets)
+       # text_Type.place(x=240, y=160)
+       text_Type.pack(pady=20)
+       date_object = datetime.date.today()
+       print(date_object.day)
+       cal1 = Calendar(tktk, selectmode = 'day',year = date_object.year, month = date_object.month,day = date_object.day)
+       cal1.pack(pady = 20)
+       cal2 = Calendar(tktk, selectmode = 'day',year = date_object.year, month = date_object.month,day = date_object.day)
+       cal2.pack(pady = 20)
+       def grad_date(x):
+              date.config(text = "Selected Date is: " + x.get_date())
+       # Add Button and Label
+       Button(tktk, text = "Get ChekIn Date",command = lambda :grad_date(cal1)).pack(pady = 20)
+       Button(tktk, text = "Get CheckOut Date",command = lambda :grad_date(cal2)).pack(pady = 20)
+       
+       date = Label(tktk, text = "")
+       date.pack(pady = 20)
+       def ReservePage():
+              if tkvar.get()=='None':
+                     popupmsg('You have to choose one of your pets to continue')
+              else:
+                     
+                     
+
+              
+
+       Button(tktk, command=, text='Submit', width=20, bg='brown', fg='white').place(x=180, y=380)
 
 def AddPetPage(USER):
        # Dictionary with options
@@ -100,8 +149,7 @@ def AddPetPage(USER):
                             popupmsg('Pet"s ID already exist')
 
 
-       x = Button(tktk, command=buttonClick, text='Submit', width=20, bg='brown',
-               fg='white').place(x=180, y=380)
+       Button(tktk, command=buttonClick, text='Submit', width=20, bg='brown', fg='white').place(x=180, y=380)
 
     # it is use for display the registration form on the window
        tktk.mainloop()
@@ -110,7 +158,7 @@ def homepageCUSTOMER(USER):
     CustomerHomePage = Toplevel(root)
     CustomerHomePage.title("Home Page")
     CustomerHomePage.geometry("200x200")
-    Button(CustomerHomePage, text="Reservation",command=lambda : AddPetPage(USER)).grid(column=0, row=1)
+    Button(CustomerHomePage, text="Reservation",command=lambda : Reservation(USER)).grid(column=0, row=1)
     Button(CustomerHomePage, text="AddPet",command=lambda : AddPetPage(USER)).grid(column=2, row=0)
     Button(CustomerHomePage, text="Quit",command=root.destroy).grid(column=0, row=0)
     signOut(CustomerHomePage)
