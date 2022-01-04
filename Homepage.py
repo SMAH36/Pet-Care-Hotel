2a546262a59c72db08313d04a2dfacea62490181
Haya177 <hayaab@ac.sce.ac.il>
2022-01-04 18:09:17 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 class pets:
     def __init__(self,root):
         self.root = root
-        self.root.geometry('1280x730+1+1')
+        self.root.state('zoomed')
         self.root.title('pets hotel')
         self.root.configure(background='#E9E9E5')
         #self.root.resizable(False,True)
@@ -22,4 +22,21 @@ class pets:
         title = Label(self.root,text='',width=20, bg='#D4D6C8',fg='black',font=('Verdana Pro Black',13)).place(x=10, y=480)
         title = Label(self.root,text='our services',width=15, bg='#D4D6C8',fg='black',font=('Verdana Pro Black',13)).place(x=230, y=420)
         #Label.pack(self.root,padx=10,ipadx=50,ipady=70)
+
 root = Tk()  
+Button(root, text='LOGIN', width=7, bg='#9A9B94',fg='black',font=('',13)).place(x=110, y=30)
+Button(root, text='SIGNUP', width=8, bg='#9A9B94',fg='black',font=('',13)).place(x=190, y=30)  
+Button(root, text= 'ADD MY PET', width=11, bg='#9A9B94',fg='black',font=('',13)).place(x=280, y=30)  
+Button(root, text='BOOK APPOINTMENT', width=18, bg='#9A9B94',fg='black',font=('',13)).place(x=400, y=30)  
+Button(root, text='ABOUT US', width=9, bg='#9A9B94',fg='black',font=('',13)).place(x=580, y=30)
+Button(root, text='TESTIMONIALS', width=13, bg='#9A9B94',fg='black',font=('',13)).place(x=680, y=30)
+Button(root, text='CONTACT US', width=11, bg='#9A9B94',fg='black',font=('',13)).place(x=820, y=30)
+Button(root, text='EXIT', width=15, bg='#5C715E',fg='black',font=('',13)).place(x=1100, y=610)
+Button(root, text='FIND US', width=8, bg='#9A9B94',fg='black',font=('',13)).place(x=940, y=30)
+#Button(root, text='LOGOUT', width=8, bg='#9A9B94',fg='black',font=('',13)).place(x=1040, y=30)
+Button(root, text='Read more...', width=11, bg='#CFDAC8',fg='black',font=('',13)).place(x=1125, y=225)
+Button(root, text='', width=2, bg='#DDBEBE',fg='black',font=('',13)).place(x=1160, y=30)
+
+op = pets(root)
+photo=PhotoImage(file="")
+root.mainloop()
