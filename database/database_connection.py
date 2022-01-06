0ee89e9e53eaa0d5e9ecadcdbd52327ab8bd920b
sahersamara <95171462+sahersamara@users.noreply.github.com>
2022-01-06 13:25:36 +0200

    a

--- database/database_connection.py
+++ database/database_connection.py
@@ -532,7 +532,26 @@ def getCustomerHistory(userId):
         cursor.close()
         connection.close()
     return result
+def getPetHistory(petId):
+    connection = connectToDb()
+    cursor = connection.cursor()
+    cursor.execute(
+        f"""
+        SELECT (room_number,start_date,end_date) FROM room_reservation WHERE pet_id = '{petId}'
+        """)
+    records = cursor.fetchall()
+    result = []
+    for record in records:
+        record = record[0].replace('(', '').replace(')', '').split(',')
+        result.append(
+            {'room_number': record[0], 'start_date': record[1], 'end_date': record[2]})
+    if (connection):
+        cursor.close()
+        connection.close()
+    return result
 
+
+print(getPetHistory('ba8beb62-5eb4-4f93-a2a0-657ba7d2a419'))
 
 # print(getCustomerHistory('e8c6d15d-b029-4ff2-90cb-b0de8a2ec38c'))
 # print(getReservationInfoByRoomNumber('1/5/22', 1))
