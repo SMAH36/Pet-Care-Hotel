d9968b0380af101ef7b6f3675e6ef0b011fa7dea
malakggh <malekgh@ac.sce.ac.il>
2022-01-06 13:19:41 +0200

    adding UnitTest

--- unit_test.py
+++ unit_test.py
@@ -45,7 +45,7 @@ class Test_functions(unittest.TestCase):
                          "not valid phone number test should return false")
 
     def test_checkIfUserExist(self):
-        self.assertEqual(checkIfUserExist('admin'), 1,
+        self.assertEqual(checkIfUserExist('a'), 1,
                          "valid database test should return true")
         self.assertEqual(checkIfUserExist('3721111'), 0,
                          "not valid database test should return false")
@@ -55,6 +55,58 @@ class Test_functions(unittest.TestCase):
     def test_register(self):
         self.assertEqual(register('dddddddd@.', '3213121', 'pass2dsa', 'what', 'ever', 25, 'Male', '3212111'), False,
                          "not valid database test should return false")
+
+    def test_getPetHistory(self):
+        self.assertEqual(getPetHistory('30a4a169-5cca-4cb7-9fc1-3ed359675f73'), [],
+                         'not vaild id should return empty list')
+
+    def test_getCustomerHistory(self):
+        self.assertEqual(getCustomerHistory('e8c6d15d-b029-4ff2-90cb-b0de8a2ec383'), [],
+                         'not vaild id should return empty list')
+
+    def test_getRoomHistory(self):
+        self.assertEqual(getRoomHistory('1')[0]['room_number'], '1',
+                         'room history 1 contains 1 should return true')
+
+    def test_getAllReservations(self):
+        self.assertEqual(getAllReservations('1/5/20'), [],
+                         'no reservations in this date should return empty list')
+
+    def test_getAllWorkers1(self):
+        self.assertEqual('31511122128' in str(getAllWorkers1()), True,
+                         'id of worker exist should return True')
+
+    def test_getAllCustomers(self):
+        self.assertEqual('3151112128' in str(getAllCustomers()), True,
+                         'id of customer exist should return True')
+
+    def test_getUncompletedTasks(self):
+        self.assertEqual(getUncompletedTasks('1/5/22', 'e8c6d15d-b029-4ff2-90cb-b0de8a2ec382'), [],
+                         'not vaild id should return empty list')
+
+    def test_getUnapprovedCompletedTasks(self):
+        self.assertEqual(getUnapprovedCompletedTasks('1/5/20'), [],
+                         'not vaild date should return empty list')
+
+    def test_approveTask(self):
+        self.assertEqual(approveTask('1/5/20', -1, 'e8c6d15d-b029-4ff2-90cb-b0de8a2ec382'), False,
+                         'not vaild data should return False')
+
+    def test_completeTask(self):
+        self.assertEqual(completeTask('1/5/20', -1, 'e8c6d15d-b029-4ff2-90cb-b0de8a2ec382'), False,
+                         'not vaild data should return False')
+
+    def test_getPetInfoByRoomNumber(self):
+        self.assertEqual('Fish' in str(getPetInfoByRoomNumber('1/5/22', '1')), True,
+                         'Fish is contained should return True')
+
+    def test_getWorkerRoomsByDate(self):
+        self.assertEqual(getWorkerRoomsByDate('1/5/22', 'e8c6d15d-b029-4ff2-90cb-b0de8a2ec382'), [],
+                         'not vaild data should return empty list')
+
+    def test_getCustomerResarvations(self):
+        self.assertEqual("('2',)" in str(getCustomerResarvations('e8c6d15d-b029-4ff2-90cb-b0de8a2ec38c')), True,
+                         '2 is contained should return True')
 
 
 if __name__ == '__main__':
